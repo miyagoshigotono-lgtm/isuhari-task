099015857e5684455b1640b291e3610f082df047
miyagoshigotono-lgtm <miyago.shigotono@gmail.com>
2026-04-16 01:08:27 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,5 +6,5 @@
 const CONFIG = {
   // GAS WebアプリのURL（doGet）
   // 例: 'https://script.google.com/macros/s/xxxxxxxxxx/exec'
-  GAS_URL: 'ここにGAS WebアプリのURLを入力',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzvsMzYeEHE89BXcR6h3uWxlO2LAD-ElfJFlP1O3IUGLmpfC0paFU7V7Ik_3r_-xOyj/exec',
 };
